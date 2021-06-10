d3933731c602a0a41d2db8ce5eb5b2f6458914e2
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-06-10 11:32:05 +0500

    Added sidebar.js

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyALV2Qr2K_1h6CjVxyAQ1y4dS41t8DBpaY",
+    authDomain: "whatsapp-3e3e1.firebaseapp.com",
+    projectId: "whatsapp-3e3e1",
+    storageBucket: "whatsapp-3e3e1.appspot.com",
+    messagingSenderId: "799831434481",
+    appId: "1:799831434481:web:3c7fb3fb30a0f7c1e57550",
+    measurementId: "G-B9RTH3799J"
+  };
