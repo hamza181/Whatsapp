2e68d5a277b4150703265b23f2c629b598456728
Ameer Hamza <ameer.hamza1811@gmail.com>
2021-06-11 00:31:22 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,20 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyALV2Qr2K_1h6CjVxyAQ1y4dS41t8DBpaY",
-    authDomain: "whatsapp-3e3e1.firebaseapp.com",
-    projectId: "whatsapp-3e3e1",
-    storageBucket: "whatsapp-3e3e1.appspot.com",
-    messagingSenderId: "799831434481",
-    appId: "1:799831434481:web:3c7fb3fb30a0f7c1e57550",
-    measurementId: "G-B9RTH3799J"
-  };
+	apiKey: 'AIzaSyALV2Qr2K_1h6CjVxyAQ1y4dS41t8DBpaY',
+	authDomain: 'whatsapp-3e3e1.firebaseapp.com',
+	projectId: 'whatsapp-3e3e1',
+	storageBucket: 'whatsapp-3e3e1.appspot.com',
+	messagingSenderId: '799831434481',
+	appId: '1:799831434481:web:3c7fb3fb30a0f7c1e57550',
+	measurementId: 'G-B9RTH3799J'
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
